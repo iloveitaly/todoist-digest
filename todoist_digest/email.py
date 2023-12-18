192ca28e7bd17d9dbc21ed27d791d230e28318fc
Michael Bianco <iloveitaly@gmail.com>
2023-12-18 10:00:03 -0700

    fix: log when email is sent

--- todoist_digest/email.py
+++ todoist_digest/email.py
@@ -1,9 +1,12 @@
+import logging
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from urllib.parse import urlparse
 
 import markdown2
+
+logger = logging.getLogger(__name__)
 
 
 def send_markdown_email(auth_url, markdown_content, subject, to_address):
@@ -19,5 +22,7 @@ def send_markdown_email(auth_url, markdown_content, subject, to_address):
     msg.attach(MIMEText(html_content, "html"))
 
     with smtplib.SMTP_SSL(parsed_url.hostname, parsed_url.port) as server:
+        logger.info("Sending email to %s", to_address)
+
         server.login(parsed_url.username, parsed_url.password)
         server.send_message(msg)
