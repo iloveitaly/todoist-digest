69c9b2064deb03459a03e246fdce6cde04dd7559
Michael Bianco <iloveitaly@gmail.com>
2023-12-21 09:24:32 -0700

    fix: use cron scheduler

--- main.py
+++ main.py
@@ -1,45 +1,48 @@
 import os
-import time
 from datetime import datetime, timedelta
+
+from apscheduler.schedulers.background import BlockingScheduler
 
 from todoist_digest import main
 
-
-def get_past_date():
-    return (datetime.utcnow() - timedelta(weeks=2)).strftime("%Y-%m-%dT%H:%M:%SZ")
+last_synced = None
 
 
-def get_next_run_timestamp(schedule):
-    tomorrow = datetime.utcnow().date() + timedelta(days=1)
-
-    return int(
-        datetime.combine(tomorrow, datetime.strptime(schedule, "%H").time()).timestamp()
-    )
+def get_initial_start_date():
+    return (datetime.utcnow() - timedelta(days=3)).strftime("%Y-%m-%dT%H:%M:%SZ")
 
 
 def is_weekday():
     return datetime.utcnow().isoweekday() < 6
 
 
+def job():
+    if not is_weekday():
+        return
+
+    global last_synced
+
+    main(
+        last_synced,
+        os.environ.get("TARGET_USER"),
+        os.environ.get("TARGET_PROJECT"),
+        os.environ.get("EMAIL_AUTH"),
+        os.environ.get("EMAIL_TO"),
+    )
+
+    last_synced = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+
+
+def cron():
+    global last_synced
+    last_synced = get_initial_start_date()
+
+    schedule = os.environ.get("SCHEDULE", "0 6 * * *")
+
+    scheduler = BlockingScheduler()
+    scheduler.add_job(job, "cron", hour=schedule)
+    scheduler.start()
+
+
 if __name__ == "__main__":
-    schedule = os.environ.get("SCHEDULE", "6")
-    last_synced = get_past_date()
-
-    while True:
-        now = int(time.time())
-        next_run = get_next_run_timestamp(schedule)
-        sleep_seconds = next_run - now
-
-        print(f"Sleeping for {sleep_seconds} seconds")
-
-        time.sleep(max(sleep_seconds, 0))
-
-        if is_weekday():
-            main(
-                last_synced,
-                os.environ.get("TARGET_USER"),
-                os.environ.get("TARGET_PROJECT"),
-                os.environ.get("EMAIL_AUTH"),
-                os.environ.get("EMAIL_TO"),
-            )
-            last_synced = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+    cron()
