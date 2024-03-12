e080730aebc7bf702a2da137dd471cb02b78c6a2
Michael Bianco <iloveitaly@gmail.com>
2024-03-12 11:47:42 -0600

    docs: output unique vars in playground
    should make this a helper / context helper

--- playground.py
+++ playground.py
@@ -8,6 +8,8 @@ from todoist_api_python.api import TodoistAPI
 from todoist_digest import *
 from todoist_digest.patch import patch_todoist_api
 
+start_keys = set(locals().keys())
+
 patch_todoist_api()
 
 api_key = os.getenv("TODOIST_API_KEY")
@@ -21,3 +23,18 @@ target_project_name = os.getenv("TARGET_PROJECT")
 last_synced_date = parser.parse("2023-12-08T16:36:30Z")
 target_user = os.getenv("TARGET_USER")
 filter_user_id = "6305953"
+
+end_keys = locals().keys()
+new_keys = set(end_keys) - start_keys
+formatted_keys = "\n - ".join(new_keys)
+
+print(
+    f"""
+Some example usage:
+
+projects = api.get_projects()
+
+Local variables:
+ - {formatted_keys}
+"""
+)
